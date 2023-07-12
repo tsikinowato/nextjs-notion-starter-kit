7f979ea77c74d3cc3fdbd971a2a1e95dcd9554dc
tsikinowato <61748619+tsikinowato@users.noreply.github.com>
2023-07-12 18:17:08 +0800

    Update site.config.ts
    add my own notion id and change the auther info

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '57f5b10d295a4f87b9b182fa3b0db8c0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Developer Pink's Log',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'PinkMiner',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
